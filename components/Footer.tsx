5a7b303b71b5ff15d2d714afd3dc2665ad408794
Oaksprout <oaksproutthetan@gmail.com>
2026-01-17 13:51:47 +0000

    All 2 child job(s) complete (delivered with terminal status)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div className="font-semibold text-primary-500">{siteMetadata.author}</div>
+          <div className="text-primary-500 font-semibold">{siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
